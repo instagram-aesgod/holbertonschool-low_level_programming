88378a58d13ed36c2443a447d3248f534fd60b1f
instagram-aesgod <vallier.ky@gmail.com>
2022-10-05 04:35:45 -0700

    test 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,27 +1,25 @@
+#include <stdio.h>
+
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
  */
-void times_table(void)
+int main(void)
 {
-	int num, mult, prod;
+	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+  
+		putchar(',');
+		putchar(' ');
+	}
 
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
+	putchar('\n');
 
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-		
+	return (0);
+}
