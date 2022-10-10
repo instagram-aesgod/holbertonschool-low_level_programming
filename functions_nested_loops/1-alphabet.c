2eb81258eb458fa6619ca00ca73122275252f8af
instagram-aesgod <vallier.ky@gmail.com>
2022-10-10 06:19:01 -0700

    test 1.1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,23 +1,14 @@
 #include "main.h"
 
 /**
- * main - /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-	char letter;
+	char alphabet;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
 
 	_putchar('\n');
-}
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
