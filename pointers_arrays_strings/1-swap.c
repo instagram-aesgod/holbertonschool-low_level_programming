762211d2e7d7e10aabc3e11bc7214fc1e942a6c3
instagram-aesgod <vallier.ky@gmail.com>
2022-10-11 02:25:57 -0700

    test 1.0

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * 1-swap.c - Swap the values of two integers
+ * @n: swap the values
+ */
+void swap_int(int *a, int *b)
+{
+	 int a, b;
+    printf("a=98 ");
+    scanf("%d", &a);
+    printf("b=42 ");
+    scanf("%d", &b);
+    swap(a, b);
+    getch();
+}
