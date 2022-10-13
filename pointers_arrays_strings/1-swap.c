6e4a2235426c76be17e5bd0f20f291198630da47
instagram-aesgod <vallier.ky@gmail.com>
2022-10-13 04:16:07 -0700

    test 1.11

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 /**
  * swap_int - Swap the values of two integers
- * @n: swap the values of a to b
- * @n: swap the values of b to a
+ * @n: swap the values
  */
 void swap_int(int *a, int *b)
+/**
+ * swap_int - check if it swaps the values
+ *@n: swap the values of a to b
+ *
+ * Return: 1 if the values have been swapped
+ */
 {
 	int change = *a;
 	*a = *b;
