5bdba02cf656d06260d8b04903e485a1ba824f20
instagram-aesgod <vallier.ky@gmail.com>
2022-10-13 16:17:21 -0700

    test 6.0

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints alternate characters
+ *
+ * @str: string to alternate print
+ *
+ * Return: always void
+ */
+void puts2(char *str)
+{
+	int counter = 0;
+
+	while (*str)
+	{
+		if (counter++ % 2 == 0)
+			_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
