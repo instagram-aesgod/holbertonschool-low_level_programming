ac4f2c4210dc9b9a450ca5d4861836a6de678f99
instagram-aesgod <vallier.ky@gmail.com>
2022-10-13 03:59:37 -0700

    test 1.8

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,26 +1,12 @@
-#include<stdio.h>
-#include<main.h>
+#include "main.h"
+#include <stdio.h>
 /**
  * 1-swap.c - Swap the values of two integers
  * @n: swap the values
  */
-void swap_int(int *a, int *b);
-void main()
+void swap_int(int *a, int *b)
 {
-    int a, b;
-    printf(" a=98 ");
-    scanf("%d", &a);
-    printf("b=42 ");
-    scanf("%d", &b);
-    swap(a, b);
-    getch();
-}
-int swap(int a, int b)
-{
-    int t;
-    t=a;
-    a=b;
-    b=t;
-    printf("\n\nThe value of a is %d.\n", a);
-    printf("The value of b is %d.\n", b);
+	int change = *a;
+	*a = *b;
+	*b = change;
 }
