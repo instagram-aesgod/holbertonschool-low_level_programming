5eae24593700c7ccebe451bfe2938e8e483503d5
instagram-aesgod <vallier.ky@gmail.com>
2023-02-28 16:54:06 -0800

    fix errors

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,26 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * rev_string - This fuction print a string reverser
- *
- *@s: this is the pointer that point to a string
- */
 
+/**
+ * rev_string - reverse a string
+ * @s:string
+ * Return:void
+ */
 void rev_string(char *s)
 {
-	int a, len;
-
-	char *begin, *end = s;
-
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+int i, j;
+char a[500];
+i = 0;
+j = 0;
+	while (*(s + i))
 	{
-		end++;
+		*(a + i) = *(s + i);
+		i++;
 	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
+	i = i - 1;
+	while (i >= 0)
 	{
-		char x;
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
+		*(s + i) = *(a + j);
+		j++;
+		i--;
 	}
-	end[len + 1] = '\0';
 }
--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,25 +1,22 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * print_array - function that prints n elements of an array of integers,
- * followed by a new line.
- * @a: This is the input array
- * @n: This is the lenght of the array
- *
- *
+ * print_array - display array
+ * @a:integer
+ * @n:integer
+ *Return:void
  */
-
 void print_array(int *a, int n)
 {
-	int index;
-	
-	for (index = 0; index < n; index++)
+int i;
+for (i = 0; i < n; i++)
 	{
-		printf("%d", a[index]);
-		if (index != (n - 1))
+	printf("%d", a[i]);
+		if (i < (n - 1))
 		{
-			printf(", ");
+		printf(", ");
 		}
 	}
-	putchar('\n');
+	printf("\n");
 }
