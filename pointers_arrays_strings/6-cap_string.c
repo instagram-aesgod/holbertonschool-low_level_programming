1df2fc44a877e6b06b9d12fa17e771caefe23486
instagram-aesgod <vallier.ky@gmail.com>
2023-03-01 23:21:58 -0800

    fix error

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,31 @@
 #include "main.h"
 
 /**
- * cap_string - Write a function that capitalizes all words of a string.
- *
- * @entry: This is the input string
- *
- * Return: String capitalized
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
  */
-char *cap_string(char *entry)
+char *cap_string(char *s)
 {
-	int conversion, index, count;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	char chars[] = {' ', ',', ';', '.', '!',
-			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
-
-	for (index = 0; entry[index] != '\0'; index++)
+	while (*(s + i))
 	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			entry[index] =  entry[index] - conversion;
-		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				conversion = 32;
-				break;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
+		i++;
 	}
-	return (entry);
+	return (s);
 }
