f472f8c956bf2c1d7d551aa59a077a9920d92a11
instagram-aesgod <vallier.ky@gmail.com>
2023-03-05 14:23:54 -0800

    last push

---
+++ pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set value of pointer a char
+ *
+ * @s: This is my entry to set
+ * @to: This is my entry to copy
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
